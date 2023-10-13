8916a5d41d6c2532b0949120a74bd62f5a811f47
Helian244 <201900828@est.umss.edu>
2023-10-13 04:30:00 -0400

    Soy gozu

---
+++ src/main/java/com/mycompany/da/Da.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.da;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Da {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(":D");
+    }
+}
